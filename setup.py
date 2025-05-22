04b876a208ccb7b15abaede632defcd2a5b415ce
krishnatadi <tadikrishna@outlook.com>
2025-05-22 19:03:28 +0530

    removed item from setup

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     name="alluuid",
     version="0.1.1",
     author="Krishna Tadi",
-    author_email="",
     description="AllUUID is a versatile Python library for generating universally unique identifiers (UUIDs). It supports multiple versions of UUIDs, including version 1 (time-based), version 4 (random), and version 7 (timestamp-based). This tool is ideal for developers looking to create unique identifiers for databases, session tokens, or any other use cases where uniqueness is critical.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
